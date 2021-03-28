a2b52a889bd8c56b99b0d855e6155c08d54baaaf
Esther Rosales <estherrosales.erb@gmail.com>
2021-03-28 17:19:43 +0000

    verion final

--- src/js/component/secondscounter.js
+++ src/js/component/secondscounter.js
@@ -6,7 +6,7 @@ import PropTypes from "prop-types";
 import "bootstrap";
 
 //include your index.scss file into the bundle
-import "/workspace/simple-counter-with-react/src/styles/index.scss";
+import "/workspace/counter-with-react-vfinal/src/styles/index.scss";
 
 export function Secondscounter(props) {
 	return (
